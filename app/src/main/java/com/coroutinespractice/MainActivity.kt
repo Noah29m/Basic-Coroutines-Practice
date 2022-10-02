d663d88d92152d1f89ea1875961dd50e7be9b50b
NOAH <noahmuhindi11@gmail.com>
2022-10-02 14:04:18 +0100

    Coroutines Practice.

---
+++ app/src/main/java/com/coroutinespractice/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.coroutinespractice
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.coroutinespractice.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    fun main() {
+        GlobalScope.launch{
+            taskb()
+        }
+        taska()
+
+    }
+
+    fun taska(){
+
+        print(" Hello")
+    }
+
+    suspend fun taskb(){
+        delay(1000L)
+        print("World")
+    }
+
+
+    }
